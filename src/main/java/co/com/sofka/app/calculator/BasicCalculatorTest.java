5f071b4bebf2f43af28862174cb28d71f7617547
NatalyMora21 <tatamora21gmail.com>
2021-09-24 10:58:43 -0500

    Created sum method and corresponding unit test

---
+++ src/main/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
@@ -0,0 +1,21 @@
+package co.com.sofka.app.calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BasicCalculatorTest {
+
+    @DisplayName("Testing several sums")
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+            "0,    1,   1",
+            "1,    2,   3",
+            "49,  51, 100",
+            "1,  100, 101"
+    })
+    public void severalSums(Long first, Long second, Long expectedResult) {
+        assertEquals(expectedResult, BasicCalculator.sum(first, second));
+    }
+}
